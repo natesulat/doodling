34b672b4c2fbede930e005f87fa706d2a38fc026
Nate Sulat <nate.sulat@gmail.com>
2019-07-18 15:58:08 -0700

    comments

--- mygrep.js
+++ mygrep.js
@@ -1,3 +1,7 @@
+// You run it like this
+//      node mygrep.js <QUERY> <FILEPATH>
+
+// So if you don't have four arguments, you're already in trouble
 if (process.argv.length !== 4) {
     console.log("Dude. You need four arguments. What am I looking for? THINK FOR A SECOND!!!");
     process.exit(1865);
@@ -11,15 +15,20 @@ var fs = require('fs'),
 
 mygrep(path, directory, query);
 
+// Take a path, a directory array, and a query
+// For all files in the directory array, execute the grep
+// If you come across a directory, recursively execute the function on it
 function mygrep(p, d, q) {
 
     var ln = d.length,
         i = 0;
 
     while (i < ln) {
-        let name = p + d[i];
+        let name = p + d[i],
             stat = fs.statSync(name);
 
+        // In Node 10+, you can use fs.Dirent objects to check this
+        // I do not have Node 10+, so this is how I did it
         if (stat.isDirectory()) {
             let dir = fs.readdirSync(name);
             mygrep(name + '/', dir);
